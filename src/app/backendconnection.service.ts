6e44136aedbcf6465f0e546c73febac441304823
Mohammad Ahtesham <md@brsindia.com>
2021-05-05 16:51:20 +0530

    leftbar changes by arsh may 5

--- src/app/backendconnection.service.ts
+++ src/app/backendconnection.service.ts
@@ -10,7 +10,7 @@ import {Router} from '@angular/router';
 export class BackendconnectionService {
 
   constructor(private httpClient : HttpClient,
-    private router: Router,
+    private router: Router
     ) { }
 
      
