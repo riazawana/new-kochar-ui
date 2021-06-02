41ccc67f91efd1002245efe801c0b77fc7c1d50a
Mohammad Ahtesham <md@brsindia.com>
2021-06-02 15:38:54 +0530

    second update on june 2nd

--- src/app/extra1/extra1.component.ts
+++ src/app/extra1/extra1.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-extra1',
   templateUrl: './extra1.component.html',
-  styleUrls: ['./extra1.component.css']
+  styleUrls: ['./extra1.component.scss']
 })
 export class Extra1Component implements OnInit {
 
